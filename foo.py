949ef8737cf11edca7480b2bc17478f29046db58
caternuson <methuzla@speakeasy.net>
2018-01-26 16:41:12 -0800

    added some actual code

--- foo.py
+++ foo.py
@@ -1,1 +1,19 @@
-print("hello world")
+"""
+`foo`
+================
+
+A bunch of foo used to learn Sphinx documenation and Read The Docs.
+
+* Author(s): Carter Nelson
+"""
+
+class Foo:
+    """A Foo class which does some stuff."""
+
+    def __init__(self):
+        self._secret = 42
+
+    def feed_pet(seld, pet):
+        """Feed the provided pet."""
+        pet.feed()
+        return True
