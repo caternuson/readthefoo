b50eaf9d863a973dc5f07a61bc46087bdf30f5f7
caternuson <methuzla@speakeasy.net>
2018-01-26 18:09:35 -0800

    added param

--- foo.py
+++ foo.py
@@ -8,12 +8,17 @@ A bunch of foo used to learn Sphinx documenation and Read The Docs.
 """
 
 class Foo:
-    """A Foo class which does some stuff."""
+    '''A Foo class which does some stuff.'''
 
     def __init__(self):
         self._secret = 42
 
-    def feed_pet(seld, pet):
-        """Feed the provided pet."""
+    def feed_pet(self, pet):
+        '''Feed the provided pet.
+
+        :param Pet pet : an instance of Pet class
+
+        :return : True if pet ate the food
+        '''
         pet.feed()
         return True
